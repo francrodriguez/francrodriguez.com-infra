1907ff28aeceea7d58b3dabd9cc07df3518a8b67
Franc Rodríguez <franc@tecob.com>
2023-01-21 15:13:52 +0100

    terraform config

---
+++ terraform/digitalocean/main.tf
@@ -0,0 +1,34 @@
+/**
+ * # DigitalOcean Project
+ */
+resource "digitalocean_project" "francrordriguezcom-infra" {
+  name        = "francrodriguezcom-infra"
+  description = "Infra para francrodriguez.com"
+  purpose     = "web"
+  environment = "production"
+  resources = digitalocean_droplet.node.*.urn
+}
+/**
+* # key a usar
+*/
+data "digitalocean_ssh_key" "one" {
+  name = "one"
+}
+/**
+ * Create one or more droplets
+ */
+resource "digitalocean_droplet" "node" {
+  count = var.node_count
+  image  = "docker-20-04"
+  name   = "node${count.index}"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  vpc_uuid    = "71bbce96-523f-4db9-b06e-f5fff6939df7"
+  ssh_keys = [data.digitalocean_ssh_key.one.id]
+}
+/**
+ * Useful to log in to the VM later
+ */
+output "nodes_ip" {
+  value = digitalocean_droplet.node.*.ipv4_address
+}
