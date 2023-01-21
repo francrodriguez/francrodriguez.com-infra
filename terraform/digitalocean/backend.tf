f631d906c858c214aee8bc5a12397518043ea3f5
Franc Rodríguez <franc@tecob.com>
2023-01-21 17:08:26 +0100

    terraform workspace

--- terraform/digitalocean/backend.tf
+++ terraform/digitalocean/backend.tf
@@ -3,7 +3,7 @@ terraform {
     hostname = "app.terraform.io"
     organization = "francrodriguez"
     workspaces {
-      name = "francrodriguezcom-infra-digitalocean"
+      name = "francrodriguezcom-infra"
     }
   }
 }
